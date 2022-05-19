fc2086ff118bdfea60ef1b7b9548bb2027bd754c
hajraladha <hajraladha@gmail.com>
2022-05-19 17:05:57 -0400

    Update

--- index.js
+++ index.js
@@ -165,7 +165,16 @@ app.delete('/users/:Username/movies/:MovieID', (req, res) => {
   Users.findOneAndUpdate({ Username: req.params.Username }, {
   $pull: { FavoriteMovies: req.params.MovieID }
   },
-  { new: true,},
+  { new: true,}, // This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
 
 //Delete user
 app.delete('/users/:Username', (req, res) => {
